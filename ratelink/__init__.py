143494ef0b4f0df7e2b87f78a51104bad32f5af6
Vladlen <76092428+vladlen-codes@users.noreply.github.com>
2026-02-01 14:56:29 +0530

    Add new backend integrations and update version
    Added support for multiple new backends including Redis, PostgreSQL, DynamoDB, and MongoDB. Updated version to 0.2.0.

--- ratelink/__init__.py
+++ ratelink/__init__.py
@@ -10,6 +10,7 @@ from .core.types import (
     ConfigError,
     TimeoutError,
 )
+
 from .core.abstractions import Algorithm, Backend, RateLimiter
 
 from .algorithms.token_bucket import TokenBucketAlgorithm
@@ -19,8 +20,30 @@ from .algorithms.fixed_window import FixedWindowAlgorithm
 from .algorithms.gcra import GCRAAlgorithm
 
 from .backends.memory import MemoryBackend
+from .backends.multi_region import MultiRegionBackend
+from .backends.custom import CustomBackendInterface
 
-__version__ = "0.1.0"
+try:
+    from .backends.redis import RedisBackend
+except ImportError:
+    pass
+
+try:
+    from .backends.postgresql import PostgreSQLBackend
+except ImportError:
+    pass
+
+try:
+    from .backends.dynamodb import DynamoDBBackend
+except ImportError:
+    pass
+
+try:
+    from .backends.mongodb import MongoDBBackend
+except ImportError:
+    pass
+
+__version__ = "0.2.0"
 
 __all__ = [
     "AlgorithmType",
@@ -42,4 +65,10 @@ __all__ = [
     "FixedWindowAlgorithm",
     "GCRAAlgorithm",
     "MemoryBackend",
+    "MultiRegionBackend",
+    "CustomBackendInterface",
+    "RedisBackend",
+    "PostgreSQLBackend",
+    "DynamoDBBackend",
+    "MongoDBBackend",
 ]
