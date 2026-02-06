209859107682357a0202f1f15a70ec9ee3e6a031
Vladlen <76092428+vladlen-codes@users.noreply.github.com>
2026-02-06 21:00:11 +0530

    Add new algorithms and backends, update version to 0.4.0
    Added new algorithms and backends to the rate limiter package, and updated the version number.

--- ratelink/__init__.py
+++ ratelink/__init__.py
@@ -11,46 +11,47 @@ from .core.types import (
     TimeoutError,
 )
 from .core.abstractions import Algorithm, Backend, RateLimiter as RateLimiterABC
-
 from .algorithms.token_bucket import TokenBucketAlgorithm
 from .algorithms.sliding_window import SlidingWindowAlgorithm
 from .algorithms.leaky_bucket import LeakyBucketAlgorithm
 from .algorithms.fixed_window import FixedWindowAlgorithm
 from .algorithms.gcra import GCRAAlgorithm
-
+from .algorithms.hierarchical import HierarchicalTokenBucket, FairQueueingAlgorithm
 from .backends.memory import MemoryBackend
 from .backends.multi_region import MultiRegionBackend
 from .backends.custom import CustomBackendInterface
-
 try:
     from .backends.redis import RedisBackend
 except ImportError:
     pass
-
 try:
     from .backends.postgresql import PostgreSQLBackend
 except ImportError:
     pass
-
 try:
     from .backends.dynamodb import DynamoDBBackend
 except ImportError:
     pass
-
 try:
     from .backends.mongodb import MongoDBBackend
 except ImportError:
     pass
-
 from .rate_limiter import RateLimiter
 from .config import ConfigLoader, RuleEngine
+from .priority_limiter import PriorityRateLimiter
+from .quota_pool import QuotaPool, SharedQuotaManager
+from .adaptive_limiter import AdaptiveRateLimiter
 
-__version__ = "0.2.0"
+__version__ = "0.4.0"
 
 __all__ = [
     "RateLimiter",
     "ConfigLoader",
     "RuleEngine",
+    "PriorityRateLimiter",
+    "QuotaPool",
+    "SharedQuotaManager",
+    "AdaptiveRateLimiter",
     "AlgorithmType",
     "BackendType",
     "WindowType",
@@ -69,6 +70,8 @@ __all__ = [
     "LeakyBucketAlgorithm",
     "FixedWindowAlgorithm",
     "GCRAAlgorithm",
+    "HierarchicalTokenBucket",
+    "FairQueueingAlgorithm",
     "MemoryBackend",
     "MultiRegionBackend",
     "CustomBackendInterface",
